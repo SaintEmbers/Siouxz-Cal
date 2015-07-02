d1429bc74b36f7054a88f41669b11c8ff0ff6d31
Jason Collins <jasoncollins@Jasons-MacBook-Air.local>
2015-07-02 15:16:58 -0700

    drag event functionality

--- calApp/calApp.js
+++ calApp/calApp.js
@@ -1,6 +1,36 @@
 CalEvent = new Mongo.Collection('calevent');
 
 if(Meteor.isClient){
+
+  Template.dialog.events({
+    "click .closeDialog": function(event, template){
+      Session.set('editing_event',null)
+    },
+    "click .updateTitle": function(evt, tmpl){
+      var title = tmpl.find('#title').value;
+      Meteor.call('updateTitle', Session.get('editing_event'),title)
+      Session.set('editing_event',null)
+    }
+  })
+  Template.main.helpers({
+    editing_event: function(){
+      return Session.get('editing_event')
+    }
+  })
+  Template.dialog.helpers({
+    title: function(){
+      var ce = CalEvent.findOne({_id:Session.get('editing_event')})
+      return cd.title
+    }
+  })
+  Template.dialog.rendered = function(){
+    if(Session.get('editDialog')){
+      var calevent = CalEvent.findOne({_id: Session.get('editDialog')})
+      if(calevent){
+        $('#title').val(calevent.title)
+      }
+    }
+  }
   Template.main.rendered = function(){
     var calendar = $('#calendar').fullCalendar({
       dayClick:function(date, allDay, jsEvent, view){
@@ -11,10 +41,18 @@ if(Meteor.isClient){
         calendarEvent.owner = Meteor.userId();
         Meteor.call('saveCalEvent',calendarEvent);
       },
+      eventClick: function(calEvent, jsEvent, view){
+        Session.set('editing_event', calEvent._id)
+      },
+      eventDrop:function(reqEvent){
+        Meteor.call('moveEvent', reqEvent)
+      },
       events: function(start, end, callback){
         var calEvents = CalEvent.find({}, {reactive:false}).fetch()
         callback(calEvents)
-      }
+      },
+      editable: true,
+      selectable: true
     }).data().fullCalendar
     Deps.autorun(function(){
       CalEvent.find().fetch();
@@ -31,10 +69,35 @@ if(Meteor.isServer) {
     Meteor.methods({
       'saveCalEvent': function(ce){
         CalEvent.insert(ce)
+      },
+      'updateTitle': function(id, title){
+        return CalEvent.update({_id:id},{$set:{title:title}})
+      },
+      'moveEvent': function(reqEvent){
+        return CalEvent.update({_id:reqEvent._id},{
+          $set:{
+            start:reqEvent.start,
+            end:reqEvent.end
+          }
+        })
       }
     })
   })
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
